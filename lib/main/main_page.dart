ad7129428ba0971e86d121caf2cffb724c250b32
pjames <pjames@gmail.com>
2023-05-07 19:25:25 +0700

    Test BottomNavigationBar

--- lib/main/main_page.dart
+++ lib/main/main_page.dart
@@ -16,54 +16,47 @@ class AppMainPage extends StatefulWidget {
 
 class _AppMainPageState extends State<AppMainPage> {
 
-  Expanded _buildLeftColumn(int flex) {
-    return Expanded(
-      flex: flex,
-      child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(10),
-              child: MainButton(
-                onPressed: () {},
-                text: "001 - LOGIN PAGE",
-              ),
-            ),
+  int _selectedIndex = 0;
 
-            Padding(
-              padding: const EdgeInsets.all(10),
-              child: MainButton(
-                onPressed: () {},
-                text: "C H A P T E R - 002",
-              ),
-            ),
+  static const TextStyle optionStyle = TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+      Text('Index 0: Home', style: optionStyle),
+      Text('Index 0: Business', style: optionStyle),
+      Text('Index 0: School', style: optionStyle)
+    ];
 
-            Padding(
-              padding: const EdgeInsets.all(10),
-              child: MainButton(
-                onPressed: () {},
-                text: "C H A P T E R - 003",
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(10),
-              child: ElevatedButton(
-                onPressed: () {},
-                child: const Text('OK'),
-              ),
-            ),
+  Widget _buildHome() {
+    return(Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Expanded(
+            flex: 1,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const [ 
+                  TestAvata(),
+                  TestCard()
+                ])),
+          const Expanded(
+            flex: 2,
+            child: TestListview()),
+          ]));
+  }
 
-            Padding(
-              padding: const EdgeInsets.all(10),
-              child: MainButton(
-                onPressed: () => {},
-                text: "C H A P T E R - 004",
-              ),
-            ),
-          ],
-        )
-      );
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  Widget _getCurrentWidget(int index)
+  {
+    if (index == 0)
+    {
+      return(_buildHome());   
+    }
+
+    return(_widgetOptions.elementAt(index));
   }
 
   @override
@@ -73,24 +66,28 @@ class _AppMainPageState extends State<AppMainPage> {
         title: const Text("Flutter Demo !!!"),
       ),
       body: Center(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Expanded(
-              flex: 1,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: const [ 
-                    TestAvata(),
-                    TestCard()
-                  ])),
-            const Expanded(
-              flex: 2,
-              child: TestListview()),
-            _buildLeftColumn(2),
-            ]
-          )
-      )
+        child: _getCurrentWidget(_selectedIndex)
+      ),
+
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            label: 'Business',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school),
+            label: 'School',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+      ),
     );
   }
 }
